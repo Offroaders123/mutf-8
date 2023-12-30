d62d147cefc096e6dcf7f475ac7f257a94c069f8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-29 17:36:08 -0800

    Super Demos
    Seeing how valid/invalid the base encoder and decoder are without specifying features for MUTF-8.

--- src/index.ts
+++ src/index.ts
@@ -36,67 +36,8 @@ export class MUtf8Decoder extends TextDecoder {
     super("utf-8",options);
   }
 
-  /**
-   * Decodes the specified bytes.
-   *
-   * @param input   - The bytes to be decoded.
-   * @param options - The options. This parameter is ignored.
-   * @returns The resultant string.
-   * @throws {TypeError} If {@link fatal} is `true` and the `input` is invalid bytes.
-   */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   override decode(input: AllowSharedBufferSource, options: TextDecodeOptions = {}): string {
-    options;
-    const buf = input instanceof Uint8Array ? input : new Uint8Array("buffer" in input ? input.buffer : input);
-    const length = buf.length;
-    const result: string[] = [];
-    let p = 0;
-    while (p < length) {
-      const b1 = buf[p++]!;
-      if (!(b1 & 0x80) && b1 !== 0) {
-        // U+0001-007F
-        result.push(String.fromCharCode(b1));
-      } else if (b1 >>> 5 === 0b110) {
-        // U+0000, U+0080-07FF
-        if (length <= p) {
-          this.#handleError(result);
-          continue;
-        }
-        const b2 = buf[p++]!;
-        if (b2 >>> 6 !== 0b10) {
-          this.#handleError(result);
-          continue;
-        }
-        result.push(String.fromCharCode(((b1 & 0x1f) << 6) | (b2 & 0x3f)));
-      } else if (b1 >>> 4 === 0b1110) {
-        // U+0800-
-        if (length <= p + 1) {
-          this.#handleError(result);
-          continue;
-        }
-        const b2 = buf[p++]!;
-        const b3 = buf[p++]!;
-        if (b2 >>> 6 !== 0b10 || b3 >>> 6 !== 0b10) {
-          this.#handleError(result);
-          continue;
-        }
-        if (b1 === 0xef && b2 === 0xbb && b3 === 0xbf && p == 3 && !this.ignoreBOM) {
-          // slip a BOM `EF BB BF`
-          continue;
-        }
-        result.push(String.fromCharCode(((b1 & 0x0f) << 12) | ((b2 & 0x3f) << 6) | (b3 & 0x3f)));
-      } else {
-        this.#handleError(result);
-      }
-    }
-    return result.join("");
-  }
-
-  #handleError(result: string[]): void {
-    if (this.fatal) {
-      throw new TypeError("MUtf8Decoder.decode: Decoding failed.");
-    }
-    result.push("\ufffd");
+    return super.decode(input,options);
   }
 }
 
@@ -122,75 +63,16 @@ export class MUtf8Encoder extends TextEncoder {
   override readonly encoding = "mutf-8";
 
   /**
-   * Encodes the specified string in MUTF-8.
-   *
-   * @param input - The string to be encoded.
-   * @returns The resultant bytes.
+   * Returns the result of running MUTF-8's encoder.
    */
-  override encode(input = ""): Uint8Array {
-    const bin: number[] = [];
-    for (const c of input) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const code: number = c.codePointAt(0)!;
-      if (0x0001 <= code && code <= 0x007f) {
-        bin.push(code);
-      } else if (code <= 0x07ff) {
-        bin.push(0xc0 | (code >>> 6));
-        bin.push(0x80 | (0x3f & code));
-      } else if (code <= 0xffff) {
-        bin.push(0xe0 | (code >>> 12));
-        bin.push(0x80 | (0x3f & (code >>> 6)));
-        bin.push(0x80 | (0x3f & code));
-      } else {
-        bin.push(0xed);
-        bin.push(0xa0 | ((code >>> 16) - 1));
-        bin.push(0x80 | (0x3f & (code >>> 10)));
-        bin.push(0xed);
-        bin.push(0xb0 | (0x0f & (code >>> 6)));
-        bin.push(0x80 | (0x3f & code));
-      }
-    }
-    return new Uint8Array(bin);
+  override encode(input?: string): Uint8Array {
+    return super.encode(input);
   }
 
   /**
-   * Encodes the specified string in MUTF-8 and stores the result to the specified array.
-   *
-   * @param source      - The string to be encoded.
-   * @param destination - The bytes to be stored the result.
-   * @returns The progress.
+   * Runs the MUTF-8 encoder on source, stores the result of that operation into destination, and returns the progress made as an object wherein read is the number of converted code units of source and written is the number of bytes modified in destination.
    */
   override encodeInto(source: string, destination: Uint8Array): TextEncoderEncodeIntoResult {
-    const destLen = destination.length;
-    let i = 0;
-    let read = 0;
-    for (const c of source) {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const code: number = c.codePointAt(0)!;
-      if (0x0001 <= code && code <= 0x007f) {
-        if (destLen <= i) break;
-        destination[i++] = code;
-      } else if (code <= 0x07ff) {
-        if (destLen <= i + 1) break;
-        destination[i++] = 0xc0 | (code >>> 6);
-        destination[i++] = 0x80 | (0x3f & code);
-      } else if (code <= 0xffff) {
-        if (destLen <= i + 2) break;
-        destination[i++] = 0xe0 | (code >>> 12);
-        destination[i++] = 0x80 | (0x3f & (code >>> 6));
-        destination[i++] = 0x80 | (0x3f & code);
-      } else {
-        if (destLen <= i + 5) break;
-        destination[i++] = 0xed;
-        destination[i++] = 0xa0 | ((code >>> 16) - 1);
-        destination[i++] = 0x80 | (0x3f & (code >>> 10));
-        destination[i++] = 0xed;
-        destination[i++] = 0xb0 | (0x0f & (code >>> 6));
-        destination[i++] = 0x80 | (0x3f & code);
-        read++;
-      }
-      read++;
-    }
-    return { read, written: i };
+    return super.encodeInto(source,destination);
   }
 }
